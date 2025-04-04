7a287cc7c5cf81cd6cc26d38b4788d24cc2f01d8
B Shrinidhi <b.shrinidhi1214@gmail.com>
2025-04-04 17:13:38 +0530

    import changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-import WaveSurfer from "./node_modules/wavesurfer.js/dist/wavesurfer.esm.js";
-import RecordPlugin from "./node_modules/wavesurfer.js/dist/plugins/record.esm.js";
+import WaveSurfer from 'wavesurfer.js'
+import RecordPlugin from 'wavesurfer.js/dist/plugins/record.esm.js'
 
 let wavesurfer, recordPlugin, startTime, wordCount = 0;
 let lastTimeCheck = Date.now(); 
